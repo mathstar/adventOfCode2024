4d25fa45aaec0e4729e46147834d2d18978d0b75
Mathias Staricka <mstaricka@gmail.com>
2024-11-30 21:30:03 -0800

    generalize whitespace

--- src/main/kotlin/com/staricka/adventofcode2024/days/Day1.kt
+++ src/main/kotlin/com/staricka/adventofcode2024/days/Day1.kt
@@ -1,14 +1,17 @@
 package com.staricka.adventofcode2024.days
 
 import com.staricka.adventofcode2024.framework.Day
+import java.util.regex.Pattern
 import kotlin.math.abs
+
+val WHITESPACE_PATTERN: Pattern = Pattern.compile("""\s+""")
 
 class Day1: Day {
     override fun part1(input: String): Int {
         val splits = input.lines()
             .filter{ it.isNotBlank() }
             .map{
-                it.split("   ")
+                it.split(WHITESPACE_PATTERN)
             }
         val left = splits.map { it[0].toInt() }.sorted()
         val right = splits.map { it[1].toInt() }.sorted()
@@ -19,7 +22,7 @@ class Day1: Day {
         val splits = input.lines()
             .filter{ it.isNotBlank() }
             .map{
-                it.split("   ")
+                it.split(WHITESPACE_PATTERN)
             }
         val left = splits.map { it[0].toInt() }
         val right = splits.map { it[1].toInt() }

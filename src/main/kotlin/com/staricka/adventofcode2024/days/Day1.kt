0ab3b681b29b8d7ed0f84bf95066d2fe5fbc2463
Mathias Staricka <mstaricka@gmail.com>
2024-11-30 21:20:36 -0800

    day 1

--- src/main/kotlin/com/staricka/adventofcode2024/days/Day1.kt
+++ src/main/kotlin/com/staricka/adventofcode2024/days/Day1.kt
@@ -1,13 +1,30 @@
 package com.staricka.adventofcode2024.days
 
 import com.staricka.adventofcode2024.framework.Day
+import kotlin.math.abs
 
 class Day1: Day {
-    override fun part1(input: String): Any? {
-        TODO("Not yet implemented")
+    override fun part1(input: String): Int {
+        val splits = input.lines()
+            .filter{ it.isNotBlank() }
+            .map{
+                it.split("   ")
+            }
+        val left = splits.map { it[0].toInt() }.sorted()
+        val right = splits.map { it[1].toInt() }.sorted()
+        return left.zip(right).sumOf { (a,b) -> abs(a - b) }
     }
 
-    override fun part2(input: String): Any? {
-        TODO("Not yet implemented")
+    override fun part2(input: String): Int {
+        val splits = input.lines()
+            .filter{ it.isNotBlank() }
+            .map{
+                it.split("   ")
+            }
+        val left = splits.map { it[0].toInt() }
+        val right = splits.map { it[1].toInt() }
+            .groupBy { it }
+            .mapValues{(_,v) -> v.size }
+        return left.sumOf { it * (right[it] ?: 0) }
     }
 }

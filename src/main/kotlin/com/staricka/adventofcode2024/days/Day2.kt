67bdd5b6d2acc26832f132e1b92a2639069baba1
Mathias Staricka <mstaricka@gmail.com>
2024-12-01 21:37:21 -0800

    day 2 proper solution

--- src/main/kotlin/com/staricka/adventofcode2024/days/Day2.kt
+++ src/main/kotlin/com/staricka/adventofcode2024/days/Day2.kt
@@ -4,56 +4,51 @@ import com.staricka.adventofcode2024.framework.Day
 import kotlin.math.abs
 
 class Day2: Day {
-    override fun part1(input: String): Any? {
+
+    private fun scan(report: List<Int>): Int? {
+        val direction = report[1] - report[0] > 0
+        for (i in 1 until report.size) {
+            if ((report[i] - report[i - 1]) > 0 != direction) {
+                return i
+            }
+            if (abs(report[i] - report[i - 1]) < 1 || abs(report[i] - report[i - 1]) > 3) {
+                return i
+            }
+        }
+        return null
+    }
+
+    override fun part1(input: String): Int {
         val reports = input.lines().filter { it.isNotEmpty() }
             .map { it.split(" ").map { level -> level.toInt() } }
 
         var count = 0
-        outer@for (report in reports) {
-            val direction = report[1] - report[0] > 0
-            for (i in 1 until report.size) {
-                if ((report[i] - report[i - 1]) > 0 != direction) {
-                    continue@outer
-                }
-                if (abs(report[i] - report[i - 1]) < 1 || abs(report[i] - report[i-1]) > 3) {
-                    continue@outer
-                }
-
+        for (report in reports) {
+            val error = scan(report)
+            if (error == null) {
+                count++
             }
-            count++
         }
         return count
     }
 
-    override fun part2(input: String): Any? {
+    override fun part2(input: String): Int {
         val reports = input.lines().filter { it.isNotEmpty() }
             .map { it.split(" ").map { level -> level.toInt() } }
 
         var count = 0
-        outer@for (report in reports) {
-            val direction = report[1] - report[0] > 0
-            var dampened = false
-            for (i in 1 until report.size) {
-                if (((report[i] - report[i - 1]) > 0) != direction) {
-                    println("${report[i-1]} ${report[i]} direction")
-                    if (dampened) {
-                        continue@outer
-                    } else {
-                        dampened = true
-                        continue
+        for (report in reports) {
+            val error = scan(report)
+            if (error == null) {
+                count++
+            } else {
+                for (i in report.indices) {
+                    if (scan(report.subList(0, i) + report.subList(i + 1, report.size)) == null) {
+                        count++
+                        break
                     }
                 }
-                if (abs(report[i] - report[i - 1]) < 1 || abs(report[i] - report[i-1]) > 3) {
-                    println("${report[i-1]} ${report[i]} diff")
-                    if (dampened) {
-                        continue@outer
-                    } else {
-                        dampened = true
-                    }
-                }
-
             }
-            count++
         }
         return count
     }

7787b54ab180be03dab0ca5b1e4ca0c30c902c19
Mathias Staricka <mstaricka@gmail.com>
2024-12-02 21:15:14 -0800

    day 3

--- src/main/kotlin/com/staricka/adventofcode2024/days/Day3.kt
+++ src/main/kotlin/com/staricka/adventofcode2024/days/Day3.kt
@@ -1,13 +1,33 @@
 package com.staricka.adventofcode2024.days
 
 import com.staricka.adventofcode2024.framework.Day
+import java.util.regex.Pattern
 
 class Day3 : Day {
-    override fun part1(input: String): Any? {
-        TODO("Not yet implemented")
+    override fun part1(input: String): Int {
+        val pattern = Pattern.compile("""mul\(([0-9]+),([0-9]+)\)""")
+        val matcher = pattern.matcher(input)
+        var result = 0
+        while (matcher.find()) {
+            result += matcher.group(1).toInt() * matcher.group(2).toInt()
+        }
+        return result
     }
 
-    override fun part2(input: String): Any? {
-        TODO("Not yet implemented")
+    override fun part2(input: String): Int {
+        val pattern = Pattern.compile("""(mul\(([0-9]+),([0-9]+)\))|(do\(\))|(don't\(\))""")
+        val matcher = pattern.matcher(input)
+        var result = 0
+        var enabled = true
+        while (matcher.find()) {
+            if (matcher.group().startsWith("mul") && enabled) {
+                result += matcher.group(2).toInt() * matcher.group(3).toInt()
+            } else if (matcher.group().startsWith("don't")) {
+                enabled = false
+            } else if (matcher.group().startsWith("do(")) {
+                enabled = true
+            }
+        }
+        return result
     }
 }
--- src/test/kotlin/com/staricka/adventofcode2024/days/Day3Test.kt
+++ src/test/kotlin/com/staricka/adventofcode2024/days/Day3Test.kt
@@ -3,8 +3,9 @@ package com.staricka.adventofcode2024.days
 class Day3Test: DayTest(
     {Day3()},
     """
-        
+       xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5)) 
     """.trimIndent(),
-    null,
-    null
+    161,
+    48,
+    part2Input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 )
